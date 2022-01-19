85dd6ccbb3280657f28f024c902494cfc210f664
Артём Валеев <48104129+artik19129@users.noreply.github.com>
2022-01-19 15:43:47 +0300

    fix

---
+++ src/app/shared/services/project.service.ts
@@ -0,0 +1,12 @@
+import {Injectable} from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ProjectService {
+  public isAuth = false;
+
+  constructor() {
+    this.isAuth = true;
+  }
+}
